3a6241205c2a5bbc81ad42da665b12f9e15fd230
Bruno <bruno_bury@hotmail.com>
2023-07-11 19:43:05 -0300

    test: test coverage, req 5, 8

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,58 +1,62 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import FilterByText from '../Components/FilterBytext';
 import PlanetContext from '../context/PlanetContext';
 import FilterByNumber from '../Components/FilterByNumber';
+import testData from './mocks/testData';
+import mockFetch from './mocks/fetch';
 
 describe('test App', () => {
   it('Verifica se o título "Planet Filter" está sendo renderizado', () => {
     render(<App />);
     const titleElement = screen.getByText(/Planet Filter/i);
-  expect(titleElement).toBeInTheDocument();
+    expect(titleElement).toBeInTheDocument();
   });
+
   it('Verifica se os componentes FilterByText, FilterByNumber e Table estão sendo renderizados', () => {
     render(<App />)
     const filterByTextElement = screen.getByTestId('name-filter');
-  expect(filterByTextElement).toBeInTheDocument();
+    expect(filterByTextElement).toBeInTheDocument();
 
-  const filterByNumberElement = screen.getByTestId('column-filter');
-  expect(filterByNumberElement).toBeInTheDocument();
+    const filterByNumberElement = screen.getByTestId('column-filter');
+    expect(filterByNumberElement).toBeInTheDocument();
 
-  const tableElement = screen.getByTestId('planet-table');
-  expect(tableElement).toBeInTheDocument();
+    const tableElement = screen.getByTestId('planet-table');
+    expect(tableElement).toBeInTheDocument();
   });
 });
 
-describe('test FilterBytext', () => {
+describe('test FilterByText', () => {
   it('teste componente FilterByText',  () => {
-  const setTextFilter = jest.fn()
+    const setTextFilter = jest.fn()
 
-  render(
-    <PlanetContext.Provider value={{  setTextFilter }}>
-      <FilterByText />
-    </PlanetContext.Provider>
-  );
+    render(
+      <PlanetContext.Provider value={{ setTextFilter }}>
+        <FilterByText />
+      </PlanetContext.Provider>
+    );
 
-  const inputElement = screen.getByTestId('name-filter');
-  expect(inputElement).toBeInTheDocument();
+    const inputElement = screen.getByTestId('name-filter');
+    expect(inputElement).toBeInTheDocument();
   
-  const inputValue = 'teste';
-  userEvent.type(inputElement, inputValue);
+    const inputValue = 'teste';
+    userEvent.type(inputElement, inputValue);
 
-  expect(inputElement.value).toEqual('teste');
+    expect(inputElement.value).toEqual('teste');
   });
 });
 
 describe('test FilterByNumber', () => {
   it('renderiza corretamente os campos de filtro', () => {
+   
     render(
       <PlanetContext.Provider
         value={{
           filterNumber: { column: 'population', comparison: 'maior que', value: '0' },
           setFilterNumber: jest.fn(),
-          planets: [],
+          planets: testData.results,
           setFilteredPlanets: jest.fn(),
           appliedFilters: [],
           setAppliedFilters: jest.fn(),
@@ -68,16 +72,20 @@ describe('test FilterByNumber', () => {
     expect(screen.getByTestId('button-filter')).toBeInTheDocument();
   });
 
-  it('atualiza corretamente os valores dos campos de filtro', () => {
+ 
+  it('chama a função de filtro ao clicar no botão', () => {
+    const setAppliedFilters = jest.fn();
+    const setFilteredPlanets = jest.fn();
+  
     render(
       <PlanetContext.Provider
         value={{
-          filterNumber: {},
+          filterNumber: { },
           setFilterNumber: jest.fn(),
-          planets: [],
-          setFilteredPlanets: jest.fn(),
+          planets: testData.results,
+          setFilteredPlanets: setFilteredPlanets,
           appliedFilters: [],
-          setAppliedFilters: jest.fn(),
+          setAppliedFilters: setAppliedFilters,
         }}
       >
         <FilterByNumber />
@@ -88,72 +96,67 @@ describe('test FilterByNumber', () => {
     userEvent.selectOptions(screen.getByTestId('comparison-filter'), ['maior que']);
     userEvent.type(screen.getByTestId('value-filter'), '1000000');
   
-    expect(screen.getByTestId('column-filter')).toHaveValue('population');
-    expect(screen.getByTestId('comparison-filter')).toHaveValue('maior que');
-    expect(screen.getByTestId('value-filter')).toHaveValue(1000000);
-  });
-
-  it('chama a função de filtro ao clicar no botão', () => {
-    const setAppliedFilters = jest.fn();
-    const setFilteredPlanets = jest.fn();
-  
-    render(
-      <PlanetContext.Provider
-        value={{
-          filterNumber: { },
-          setFilterNumber: jest.fn(),
-          planets: [],
-          setFilteredPlanets: setFilteredPlanets,
-          appliedFilters: [],
-          setAppliedFilters: setAppliedFilters,
-        }}
-      >
-        <FilterByNumber />
-      </PlanetContext.Provider>
-    );
-  
-    userEvent.selectOptions(screen.getByTestId('column-filter'), ['population']);
-    userEvent.selectOptions(screen.getByTestId('comparison-filter'), ['maior que']);
-    userEvent.type(screen.getByTestId('value-filter'), 1000000);
-  
     userEvent.click(screen.getByTestId('button-filter'));
   
     expect(setAppliedFilters).toHaveBeenCalled();
     expect(setFilteredPlanets).toHaveBeenCalled();
   });
-  it('aplica o filtro corretamente', () => {
-    const planets = [
-      { population: 1000000 },
-      { population: 2000000 },
-      { population: 3000000 },
-    ];
 
-    const setAppliedFilters = jest.fn();
-    const setFilteredPlanets = jest.fn();
 
-    render(
-      <PlanetContext.Provider
-        value={{
-          filterNumber: { column: 'population', comparison: 'maior que', value: '1500000' },
-          setFilterNumber: jest.fn(),
-          planets: planets,
-          setFilteredPlanets: setFilteredPlanets,
-          appliedFilters: [],
-          setAppliedFilters: setAppliedFilters,
-        }}
-      >
-        <FilterByNumber />
-      </PlanetContext.Provider>
-    );
+ it('chama o fetch com sucesso e exibe os dados da tabela', async () => {    
+    jest.spyOn(global, 'fetch').mockImplementation(mockFetch);
+    await act(async () => {
+      render(<App />);
+    }); 
+    const tableRows = await screen.findAllByRole('row');
+    expect(tableRows.length).toBe(11);     
+    global.fetch.mockRestore();
+  });
 
-    userEvent.click(screen.getByTestId('button-filter'));
 
-    expect(setFilteredPlanets).toHaveBeenCalledWith([
-      { population: 2000000 },
-      { population: 3000000 },
-    ]);
-    expect(setAppliedFilters).toHaveBeenCalledWith([
-      { column: 'population', comparison: 'maior que', value: '1500000' },
-    ]);
-  });  
+  it('chama o fetch com sucesso e exibe os dados da tabela (igual a)', async () => {
+    jest.spyOn(global, 'fetch').mockImplementation(mockFetch);
+    await act(async () => {
+      render(<App />);
+    });
+
+    const columnFilter = screen.getByTestId('column-filter');
+    userEvent.selectOptions(columnFilter, ['population']);
+
+    const comparisonFilter = screen.getByTestId('comparison-filter');
+    userEvent.selectOptions(comparisonFilter, ['igual a']);
+
+    const valueFilter = screen.getByTestId('value-filter');
+    userEvent.type(valueFilter, '4500000000');
+
+    const buttonFilter = screen.getByTestId('button-filter');
+    userEvent.click(buttonFilter);
+
+    const tableRows = await screen.findAllByRole('row');
+    expect(tableRows.length).toBe(2); 
+  });
+  it('chama o fetch com sucesso e exibe os dados da tabela (menor que)', async () => {
+    jest.spyOn(global, 'fetch').mockImplementation(mockFetch);
+    await act(async () => {
+      render(<App />);
+    });
+
+    const columnFilter = screen.getByTestId('column-filter');
+    userEvent.selectOptions(columnFilter, ['population']);
+
+    const comparisonFilter = screen.getByTestId('comparison-filter');
+    userEvent.selectOptions(comparisonFilter, ['menor que']);
+
+    const valueFilter = screen.getByTestId('value-filter');
+    userEvent.type(valueFilter, '200000');
+
+    const buttonFilter = screen.getByTestId('button-filter');
+    userEvent.click(buttonFilter);
+
+    const tableRows = await screen.findAllByRole('row');
+    expect(tableRows.length).toBe(2); 
+  });
 });
+  
+
+
